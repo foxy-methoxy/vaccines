98db08e3c1c210d3d42742b5e4fa0f359858b54b
OliwiaOremek <oliwia.oremek@thecodest.co>
2022-05-10 14:22:07 +0200

    setup i18n

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloI18n />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloI18n from "./components/HelloI18n.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    HelloI18n,
   },
 });
 </script>
